71a335b96c2ba2ff77285be9871f9885cdd72a6a
tatsuya.atsumi <tatsuya.atsumi@brainpad.co.jp>
2018-09-17 15:10:04 +0900

    テンプレ生成〜カウンタの実装

---
+++ components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <button @click="$store.commit('increment')">increment</button>
+  </div>
+</template>
+
+<script>
+    export default {
+      computed: {
+        count() {
+          return this.$store.state.counter
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
